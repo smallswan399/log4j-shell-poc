a55a9f1ccc3fcbf56b580784339c47df9cce3a3b
debian-12 <debian12@debian12>
2023-07-01 20:08:30 -0400

    Update vulnerable app

--- vulnerable-application/src/main/java/com/example/log4shell/LoginServlet.java
+++ vulnerable-application/src/main/java/com/example/log4shell/LoginServlet.java
@@ -11,6 +11,8 @@ import org.apache.logging.log4j.Logger;
 @WebServlet(name = "loginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
 
+    Logger logger = LogManager.getLogger(LoginServlet.class);
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -18,18 +20,17 @@ public class LoginServlet extends HttpServlet {
         String password = req.getParameter("password");
 
         // vulnerable code
-        Logger logger = LogManager.getLogger(LoginServlet.class);
         String agent = req.getHeader("User-Agent");
-        logger.info(agent);
+        logger.error(agent);
+        logger.info("Log some info here");
+        logger.warn("Log some warn here");
 
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("<html><body>");
 
         if (userName.equals("admin") && password.equals("password")) {
             out.println("Welcome Back Admin");
         } else {
-            logger.error(userName);
             out.println("<code> the password you entered was invalid, <u> we will log your information </u> </code>");
         }
     }
